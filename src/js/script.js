d8191c23d4b7e00c2c5c16b17d9bea6ee0ca8016
NERFTHIS <mustaevs@gmail.com>
2023-12-02 13:46:42 +0300

    Initialize rooms

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,20 @@ class Model {
     map: {
       width: 40,
       height: 24,
-      // mapTiles will be represented as an array of objects:
+      // tiles will be represented as an array of objects:
       // { type: String, x: Number, y: Number }
       tiles: [],
+      rooms: {
+        roomsNumber: 0,
+        minRoomsNumber: 5,
+        maxRoomsNumber: 10,
+        minRoomSize: 3,
+        maxRoomSize: 8,
+        // roomsCoords will be represented as an array of arrays of coordinate of a
+        // separate room:
+        // [leftCornerX, leftCornerY, rightCornerX, rightCornerY]
+        roomsCoords: [],
+      },
     },
   };
 
@@ -17,6 +28,72 @@ class Model {
         map.tiles.push({ type: 'tile-w', x: i, y: j });
       }
     }
+  }
+
+  addRoomsOnMapTiles() {
+    const { tiles } = this.state.map;
+    const { roomsCoords } = this.state.map.rooms;
+
+    this._initRooms();
+
+    for (const [leftX, leftY, rightX, rightY] of roomsCoords) {
+      for (let j = leftY; j <= rightY; j++) {
+        for (let i = leftX; i <= rightX; i++) {
+          const tile = tiles.find(tile => tile.x === i && tile.y === j);
+          tile.type = 'tile';
+        }
+      }
+    }
+  }
+
+  _initRooms() {
+    this._initRoomsNumber();
+
+    const { roomsNumber, roomsCoords } = this.state.map.rooms;
+    for (let i = 0; i < roomsNumber; i++) {
+      const roomCoords = this._calcRoomCoords();
+
+      roomsCoords.push(roomCoords);
+    }
+  }
+
+  _initRoomsNumber() {
+    const { rooms } = this.state.map;
+
+    rooms.roomsNumber = this._getRandomInt(
+      rooms.minRoomsNumber,
+      rooms.maxRoomsNumber
+    );
+  }
+
+  _calcRoomCoords() {
+    const { width, height } = this.state.map;
+    const { minRoomSize, maxRoomSize } = this.state.map.rooms;
+
+    const roomWidth = this._getRandomInt(minRoomSize, maxRoomSize);
+    const roomHeight = this._getRandomInt(minRoomSize, maxRoomSize);
+
+    const leftCoordX = this._getRandomInt(0, width - 1);
+    const leftCoordY = this._getRandomInt(0, height - 1);
+
+    const rightCoordX = this._adjustRoomRightCornerCoord(
+      leftCoordX + roomWidth,
+      width
+    );
+    const rightCoordY = this._adjustRoomRightCornerCoord(
+      leftCoordY + roomHeight,
+      height
+    );
+
+    return [leftCoordX, leftCoordY, rightCoordX, rightCoordY];
+  }
+
+  _adjustRoomRightCornerCoord(rightCornerCoord, mapSize) {
+    return rightCornerCoord >= mapSize ? mapSize - 1 : rightCornerCoord;
+  }
+
+  _getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
   }
 }
 
@@ -86,6 +163,7 @@ class Controller {
 
   _init() {
     this._model.initMapTiles();
+    this._model.addRoomsOnMapTiles();
     this._mapView.addHandlerRender(this._controlMap.bind(this));
   }
 
