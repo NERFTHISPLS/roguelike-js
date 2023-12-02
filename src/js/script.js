835d39fd0da82dc7bf1b3137598fa2df516640d1
NERFTHIS <mustaevs@gmail.com>
2023-12-02 12:48:02 +0300

    Initialize the game map

---
+++ src/js/script.js
@@ -0,0 +1,97 @@
+class Model {
+  state = {
+    map: {
+      width: 40,
+      height: 24,
+      // mapTiles will be represented as an array of objects:
+      // { type: String, x: Number, y: Number }
+      tiles: [],
+    },
+  };
+
+  initMapTiles() {
+    const { map } = this.state;
+
+    for (let j = 0; j < map.height; j++) {
+      for (let i = 0; i < map.width; i++) {
+        map.tiles.push({ type: 'tile-w', x: i, y: j });
+      }
+    }
+  }
+}
+
+class View {
+  _data;
+
+  render(data) {
+    this._data = data;
+    const markup = this._generateMarkup();
+
+    this._insert(markup);
+  }
+
+  _insert(markup) {
+    this._clear();
+    this._parentElement.insertAdjacentHTML('beforeend', markup);
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+}
+
+class MapView extends View {
+  _parentElement = document.querySelector('.field');
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  _generateMarkup() {
+    const { width, height, tiles } = this._data;
+
+    const style = {
+      gridTemplateColumns: `repeat(${width}, 1fr)`,
+      gridTemplateRows: `repeat(${height}, 1fr)`,
+    };
+
+    this._addStyleToParent(style);
+
+    const markup = tiles
+      .map(
+        tile => /* html */ `
+          <div class=${tile.type} data-x="${tile.x}" data-y="${tile.y}"></div>
+        `
+      )
+      .join('\n');
+
+    return markup;
+  }
+
+  _addStyleToParent({ gridTemplateColumns, gridTemplateRows }) {
+    this._parentElement.style.gridTemplateColumns = gridTemplateColumns;
+    this._parentElement.style.gridTemplateRows = gridTemplateRows;
+  }
+}
+
+class Controller {
+  _model = new Model();
+  _mapView = new MapView();
+
+  _state = this._model.state;
+
+  constructor() {
+    this._init();
+  }
+
+  _init() {
+    this._model.initMapTiles();
+    this._mapView.addHandlerRender(this._controlMap.bind(this));
+  }
+
+  _controlMap() {
+    this._mapView.render(this._state.map);
+  }
+}
+
+const game = new Controller();
